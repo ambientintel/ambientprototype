6a205c1e5c9808ef115bad97878096cd4587ece9
john3913 <155326347+john3913@users.noreply.github.com>
2026-04-29 11:57:54 -0500

    fix: update upper-left logo link to go to home page (/)

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -99,7 +99,7 @@ export default function OverviewPage() {
 
       {/* Sidebar */}
       <aside className="sidebar">
-        <Link href="/home" style={{ textDecoration:'none', color:'inherit' }}>
+        <Link href="/" style={{ textDecoration:'none', color:'inherit' }}>
           <div className="brand">
             <div className="brand-name">Ambient <em>Demo</em></div>
           </div>
